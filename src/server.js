fbe13fc2dc4e9a53958e7ea14483f6a5067c9afa
u293742a-commits <u293742a@gmail.com>
2026-09-21 14:52:15 +0500

    Revert "Extract actual source files from uploaded zip archive"

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`BrickMaster ERP API running on port ${PORT} [${process.env.NODE_ENV || 'development'}]`);
-});
